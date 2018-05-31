9752cc45009febfdefbcc9107d6d373c5bf26392
yansongda <me@yansongda.cn>
2018-05-31 09:30:58 +0800

    update

--- test.py
+++ test.py
@@ -3,19 +3,21 @@
 from aliyun_iot_device.mqtt import Client
 import time
 
-PRODUCE_KEY = "b1VzFx30hEm"
-DEVICE_NAME = "iot_device_01"
-DEVICE_SECRET = "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"
-CLIENT_ID = ""
+print(Client.__file__)
 
-PUBLISH_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/update"
-SUBSCRIBE_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/get"
+# PRODUCE_KEY = "b1VzFx30hEm"
+# DEVICE_NAME = "iot_device_01"
+# DEVICE_SECRET = "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"
+# CLIENT_ID = ""
 
-iot = Client(("b1VzFx30hEm", "iot_device_01", "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"), "yansongda", tls=False)
+# PUBLISH_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/update"
+# SUBSCRIBE_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/get"
 
-iot.connect()
+# iot = Client(("b1VzFx30hEm", "iot_device_01", "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"), "yansongda", tls=False)
 
-iot.mqtt.loop_start()
-while True:
-    iot.mqtt.publish(PUBLISH_TOPIC, 'success', 1)
-    time.sleep(5)
+# iot.connect()
+
+# iot.mqtt.loop_start()
+# while True:
+#     iot.mqtt.publish(PUBLISH_TOPIC, 'success', 1)
+#     time.sleep(5)
