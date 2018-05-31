fc092ecaa7469de1143dd1c28bd8ab885a67d998
yansongda <me@yansongda.cn>
2018-05-31 09:51:33 +0800

    update

--- aliyun_iot_device/mqtt/client.py
+++ aliyun_iot_device/mqtt/client.py
@@ -17,31 +17,50 @@ DEFAULT_PUBLISH_TOPIC = "/{product_key}/{device_name}/update"
 DEFAULT_SUBSCRIBE_TOPIC = "/{product_key}/{device_name}/get"
 
 KEEPALIVE = 60
+CA_CERTS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'root.cer')
 
 
 class Client(object):
     """阿里云 IOT 套件 MQTT 客户端
     """
 
-    def __init__(self, product_device, client_id=None, region="cn-shanghai", domain_direct=True, ca_certs=None):
+    def __init__(self, product_device, client_id=None,
+                 region="cn-shanghai", domain_direct=True, tls=True, ca_certs=CA_CERTS):
         super(Client, self).__init__()
         if not isinstance(product_device, tuple):
             raise TypeError('{pd} Must Be A Tuple'.format(pd=product_device))
 
+        if client_id is None:
+            client_id = str(round(time.time() * 1000))
+
         self.client_id = client_id
         self.region = region
+        self.tls = tls
         self.ca_certs = ca_certs
         self.domain_direct = domain_direct
         self.product_key, self.device_name, self.device_secret = product_device
 
-        self.mqtt_uri = DOMAIN_DIRECT_URI.format(product_key=self.product_key, region=region)
-        self.mqtt_port = DOMAIN_DIRECT_PORT
-        self.mqtt = self.get_mqtt_client()
+        self.mqtt = self._get_mqtt_client()
 
     def connect(self, keepalive=KEEPALIVE):
         return self.mqtt.connect(self.mqtt_uri, self.mqtt_port, keepalive)
 
-    def get_mqtt_client(self):
+    def publish(self, topic, payload=None, qos=0, retain=False):
+        return self.mqtt.publish(topic, payload, qos, retain)
+
+    def subscribe(self, topic, qos=0):
+        return self.mqtt.subscribe(topic, qos)
+
+    def unsubscribe(self, topic):
+        return self.mqtt.unsubscribe(topic)
+
+    def loop_start(self):
+        return self.mqtt.loop_start()
+
+    def loop_stop(self, force=False):
+        return self.mqtt.loop_stop(force)
+
+    def _get_mqtt_client(self):
         if self.domain_direct:
             mqtt_client_id, mqtt_user, mqtt_passwd = self._get_doamin_direct_mqtt_info()
         else:
@@ -49,20 +68,23 @@ class Client(object):
 
         mqtt = mqtt_client.Client(mqtt_client_id)
         mqtt.username_pw_set(mqtt_user, mqtt_passwd)
-        if self.ca_certs:
+        if self.tls:
             mqtt.tls_set(ca_certs=self.ca_certs)
 
         return mqtt
 
     def _get_doamin_direct_mqtt_info(self):
         mode = "3"
-        if self.ca_certs:
+        if self.tls:
             mode = "2"
 
         mqtt_client_id = self.client_id + "|securemode=" + mode + ",signmethod=hmacsha1,timestamp=" + str(round(time.time())) + "|"
         mqtt_user = self.device_name + "&" + self.product_key
         mqtt_content = "clientId" + self.client_id + "deviceName" + self.device_name + "productKey" + self.product_key + "timestamp" + str(round(time.time()))
         mqtt_passwd = hmac.new(bytes(self.device_secret, 'utf-8'), bytes(mqtt_content, 'utf-8'), hashlib.sha1).hexdigest()
+
+        self.mqtt_uri = DOMAIN_DIRECT_URI.format(product_key=self.product_key, region=self.region)
+        self.mqtt_port = DOMAIN_DIRECT_PORT
 
         return mqtt_client_id, mqtt_user, mqtt_passwd
 
--- test.py
+++ test.py
@@ -3,21 +3,20 @@
 from aliyun_iot_device.mqtt import Client
 import time
 
-print(Client.__file__)
 
-# PRODUCE_KEY = "b1VzFx30hEm"
-# DEVICE_NAME = "iot_device_01"
-# DEVICE_SECRET = "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"
-# CLIENT_ID = ""
+PRODUCE_KEY = "b1VzFx30hEm"
+DEVICE_NAME = "iot_device_01"
+DEVICE_SECRET = "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"
+CLIENT_ID = ""
 
-# PUBLISH_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/update"
-# SUBSCRIBE_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/get"
+PUBLISH_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/update"
+SUBSCRIBE_TOPIC = "/" + PRODUCE_KEY + "/" + DEVICE_NAME + "/get"
 
-# iot = Client(("b1VzFx30hEm", "iot_device_01", "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"), "yansongda", tls=False)
+iot = Client(("b1VzFx30hEm", "iot_device_01", "3TSqd6sfzjSkSwEmLmcAdZnI0oGlmRZ8"))
 
-# iot.connect()
+iot.connect()
 
-# iot.mqtt.loop_start()
-# while True:
-#     iot.mqtt.publish(PUBLISH_TOPIC, 'success', 1)
-#     time.sleep(5)
+iot.mqtt.loop_start()
+while True:
+    iot.mqtt.publish(PUBLISH_TOPIC, 'success', 1)
+    time.sleep(5)
